701528a9ff449a251446b0e7589bf91d32cd58da
blthree <blthree89@gmail.com>
2017-10-19 19:24:30 -0400

    refactor redirect into openFile

--- index2.js
+++ index2.js
@@ -2,16 +2,19 @@ var fs = require('fs');
 
 function openFile () {
  dialog.showOpenDialog({ filters: [{ name: 'text', extensions: ['tab'] }
-  ]}, function (fileNames) {
+  ]}, function (fileNames, err) {
   if (fileNames === undefined) return;
   var fileName = fileNames[0];
   var cookie = {url: 'http://www.benson.fyi', name: 'fileName', value: fileName}
   console.log(cookie)
   session.defaultSession.cookies.set(cookie, (error) => {
     if (error) console.error(error)
+    location.href='file://' + __dirname + '/index.html'
 })
- }); 
+ });
+ 
 }
 
 
+document.getElementById('openFileButton').addEventListener('click', openFile);
 
