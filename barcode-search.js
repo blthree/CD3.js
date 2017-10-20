ceec11130ad49974184de79cfbd3aee50ed6b481
Benson Lindsey <blthree89@gmail.com>
2017-10-19 21:33:49 -0400

    improvements to data store, refactored into fewer js files

--- barcode-search.js
+++ barcode-search.js
@@ -1,6 +1,7 @@
     var loadtsv = require("./load-tsv")
     var app = require('electron').remote; 
     var dialog = app.dialog;
+    var session = app.session;
 
     function test_func () {
         document.querySelector(".search-code").value = document.getElementById("sample-barcode").textContent;
@@ -10,13 +11,14 @@
     function search_func () {
         var searchCode = document.querySelector(".search-code").value;
         console.log(searchCode);
-        document.getElementById('search-results').textContent = JSON.stringify(store.get('barcode_data.' + searchCode));
+
+            document.getElementById('search-results').textContent = JSON.stringify(store.get('barcode_data.' + searchCode));
         document.getElementById('search-box').value = "";
         document.getElementById('search-box').focus();
     } 
-
     document.querySelector("[id='testB']").addEventListener('click', search_func);
     document.getElementById("search-box").addEventListener('keypress', function (e) { if (e.keyCode === 13) { search_func(); } });
+
 
 
 
--- load-barcodes.js
+++ load-barcodes.js
@@ -5,13 +5,45 @@ var session = app.session;
 const Store = require('electron-store');
 const store = new Store();
 
+function loadtsv(fileName, callback) {
+  var fs = require('fs');
+  data = fs.readFile(fileName, function (err, data) {
+      if (err) {
+          console.log(err);
+          return(callback(err));
+      }
+      data = data.toString().replace(/\r/g, '').split('\n');
+      json_data = {}
+      for (i =0; i < data.length; i++) {
+          line = data[i].split("\t");
+          json_data[line[0]] = {"StockNum": line[1], "CloneName": line[2], "Box": line[3], "Position": line[4]};
+          //console.log(json_data)
+      }
+      callback(null, json_data);
+  });
+
+};
+
 function openFile () {
  dialog.showOpenDialog({ filters: [{ name: 'text', extensions: ['tab'] }
   ]}, function (fileNames, err) {
   if (fileNames === undefined) return;
   var fileName = fileNames[0];
   store.set('fileName2',fileName)
-  location.href='file://' + __dirname + '/barcode-search.html';
+  // save the date and time the electron-stre was last updated
+  date = new Date()
+  store.set('updated', date.toGMTString())
+  console.log(store.get('fileName2'))
+  // load and parse data from file, than store using electron-store
+  loadtsv(store.get('fileName2'), function (err, data) {
+    if (err) { console.log(err)};
+    // clear current data
+    store.set('barcode_data', {})
+    // save the new data
+    store.set('barcode_data', data)
+  })
+  // reset cursor focus to search box
+  document.getElementById('search-box').focus();
  });
 }
 
