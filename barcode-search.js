9ec55dcfa40813701fde46467480783be983bf41
Benson Lindsey <blthree89@gmail.com>
2017-10-19 21:13:11 -0400

    pre-load barcode data into electron-store

--- barcode-search.js
+++ barcode-search.js
@@ -1,9 +1,6 @@
     var loadtsv = require("./load-tsv")
     var app = require('electron').remote; 
     var dialog = app.dialog;
-    var session = app.session;
-   // const Store = require('electron-store');
-   // const store = new Store();
 
     function test_func () {
         document.querySelector(".search-code").value = document.getElementById("sample-barcode").textContent;
@@ -13,13 +10,11 @@
     function search_func () {
         var searchCode = document.querySelector(".search-code").value;
         console.log(searchCode);
-        loadtsv(store.get('fileName2'), function (err, data) {
-        console.log(data[searchCode]);
-        document.getElementById('search-results').textContent = JSON.stringify(data[searchCode]);
-        })
+        document.getElementById('search-results').textContent = JSON.stringify(store.get('barcode_data.' + searchCode));
         document.getElementById('search-box').value = "";
         document.getElementById('search-box').focus();
     } 
+
     document.querySelector("[id='testB']").addEventListener('click', search_func);
     document.getElementById("search-box").addEventListener('keypress', function (e) { if (e.keyCode === 13) { search_func(); } });
 
